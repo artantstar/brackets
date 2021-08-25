f90501dda98124133243e0dac068a376494ef87e
artantstar <artantstar@gmail.com>
2021-08-25 21:12:56 +0300

    feat: complete task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,24 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  let str2 = str.split('');
+  let brackets = bracketsConfig;
+  let arr = [];
+
+  for (let i = 0; i < str2.length; i++) {
+    for (let j = 0; j < brackets.length; j++) {
+      if (!(str2[i] === brackets[j][1] && arr[arr.length - 1] === brackets[j][0])) {
+        if (str2[i] === brackets[j][0])
+          arr.push(str[i]);
+      } else arr.pop();
+    }
+  }
+
+  if (str2.length % 2 !== 0) {
+    return false;
+  }
+  if (arr.length === 0) {
+    return true;
+  }
+  else {
+    return false;
+  }
 }
